bbaa87719169e80466066153262daa9f80b25239
JohvanyROB <johvany.gustave@gmail.com>
2023-11-08 12:00:37 +0100

    code commented

--- tello_controller/aruco_focus.py
+++ tello_controller/aruco_focus.py
@@ -1,5 +1,3 @@
-import time
-
 import rclpy
 from rclpy.node import Node
 
@@ -9,7 +7,8 @@ from sensor_msgs.msg import Image
 from tello_msgs.srv import TelloAction
 from rclpy.qos import qos_profile_sensor_data
 from cv_bridge import CvBridge, CvBridgeError  # Package to convert between ROS and OpenCV Images
-import cv2  # OpenCV library
+
+import cv2
 import cv2.aruco as aruco
 
 from Pid import PID
@@ -17,7 +16,7 @@ from Pid import PID
 
 class ActionManager(Node):
     def __init__(self):
-        super().__init__('controller_manager')
+        Node.__init__(self, 'controller_manager')
         self.cli = self.create_client(TelloAction, '/drone1/tello_action')
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
@@ -33,33 +32,30 @@ class ActionManager(Node):
 
 
 class Controller(Node):
-
     def __init__(self):
-        super().__init__('controller')
+        Node.__init__(self, 'controller')
 
-        #aruco config
-        self.aruco_detector = aruco.ArucoDetector(aruco.getPredefinedDictionary(aruco.DICT_6X6_250), aruco.DetectorParameters())
+        self.aruco_detector = aruco.ArucoDetector(aruco.getPredefinedDictionary(aruco.DICT_6X6_250), aruco.DetectorParameters())    #aruco config
 
-        ### CREATE THE PUBLISHER FOR COMMAND VELOCITY
-        self.cmd_vel_publisher_ = self.create_publisher(Twist, '/drone1/cmd_vel', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.cmd_vel_loop)
-        # Init desired speed
+        self.cmd_vel_publisher_ = self.create_publisher(Twist, '/drone1/cmd_vel', 10)   #publisher for velocity commands
+        self.create_timer(0.5, self.cmd_vel_loop)   #cmd_vel_loop called each 0.5s
+
+        # Init desired velocities
         self.vx = 0.0
         self.vy = 0.0
         self.vz = 0.0
         self.v_yaw = 0.0
 
-        ### CREATE THE SUBSCRIBER FOR IMAGE RECEPTION
-        self.image_subscription_ = self.create_subscription(Image, '/drone1/image_raw', self.image_callback, 10)
+        self.image_subscription_ = self.create_subscription(Image, '/drone1/image_raw', self.image_callback, qos_profile=qos_profile_sensor_data)   #subscriber for image
         self.display_image = True
-        # Used to convert between ROS and OpenCV images
-        self.br = CvBridge()
+        
+        self.br = CvBridge()    # Used to convert between ROS and OpenCV images
+
 
     def cmd_vel_loop(self):
         """
         I'm used to publish every 0.5 seconds the velocity command.
-        You don't need to modify me, just modify vx, vy and v_yaw when you need it.
+        You don't need to modify it, just modify vx, vy and v_yaw when you need it.
         """
         msg = Twist()
         msg.linear.x = self.vx
@@ -68,17 +64,13 @@ class Controller(Node):
         msg.angular.z = self.v_yaw
         self.cmd_vel_publisher_.publish(msg)
 
+
     def image_callback(self, data):
-        """
-        I'm used to get camera's image.
-        I can set the desired velocity according to the aruco detection
-        """
+        """ Method called whenever a new image is published on /drone1/image topic """
         try:
             cv_image = self.br.imgmsg_to_cv2(data)
-            # Our operations on the frame come here
             gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
-            # lists of ids and the corners beloning to each id
             corners, ids, _ = self.aruco_detector.detectMarkers(gray)
 
             gray = aruco.drawDetectedMarkers(gray, corners)
@@ -90,7 +82,7 @@ class Controller(Node):
                 #Like self.vx, self.vy, self.v_yaw, to control the UAV
             else:
                 print("No tag seen..")
-                #The UAV stay in the same pose, it doesn't move
+                #The UAV stays in the same pose, it doesn't move
                 self.vx = 0.0
                 self.vy = 0.0
                 self.vz = 0.0
